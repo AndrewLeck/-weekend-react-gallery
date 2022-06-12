5e1e3cda9d63cd64efaf66a3b191fe7e822a7bd9
Andrew Leck <Aleck360@gmail.com>
2022-06-12 14:52:02 -0500

    added 4 images to the gallery.data

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,9 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    {id: 2, title:'Naruto', description:'Photo of Itachi taken while apart of the akatsuki ', path: 'images/itachi.webp'},
+    {id: 3, title:'Naruto', description:'Photo of Sauske taken when he was younger ', path: 'images/Sauske.png' },
+    {id: 4, title:'Naruto', description:'Photo of Naruto taken while in sage mode ', path: 'images/Naruto_sage_mode.webp'},
+    {id: 5, title:'Naruto', description:'Photo of Rock Lee taken while he was younger ', path: 'images/rock_lee.jpeg'}
 ];
 
 module.exports = galleryItems;
